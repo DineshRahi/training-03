03e955490a1084f6e9c2a736208feb26c5c090c4
DineshRahi <157383107+DineshRahi@users.noreply.github.com>
2025-01-09 11:54:36 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,55 @@
+// arthmetic operators 
+// console.log(10+10);//additions 
+// console.log("10" + 10);
+// adding two or more string called concadination
+// console.log("JS"+50+50);JS 5050
+// console.log("JS"+(50+50));JS 5050  
+
+// subtraction 
+// console.log(10-5);
+// console.log("10"-5);
+// console.log("10" - "hi");
+// console.log("10" - "5");
+// console.log("5"*5);
+// //! funtional without parameters
+// functional DeviceMotionEvent(){
+//     console.log("functional without parameters";
+//             )
+// }
+// function UserDetails(name , age, city){
+//     console.log(name);
+//     console.log(age);
+//     console,log(city);
+//     }
+// UserDetails("yeshwanth",32,hyd)
+//  //!anaymous function 
+//  function(){
+//     console.log("AF";
+
+//     )
+//  }
+
+//  //! function expression 
+//  let x=function(){
+//     console.log("function exporession is executing");
+    
+//  }
+//  //! LIFE
+// ( function(){
+//     console.log("IIFE");
+//  })();
+ // ; is used terminate or end the statement
+// //! arrow function
+// function demo() {
+//     console.log("Helo");
+// }();
+// let x==> console.log(a);
+// x(5);
+// let x=(a,b)=>console.log(a+b);
+// x(5);
+// let x=_=>{console.log("Hi");
+//     console.log("Helo");
+//     console.log("Bye");}
+//     x();
+    
+for a<5
